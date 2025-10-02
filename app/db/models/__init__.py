5ac4869603befc467bff3e13bae4da37e3d191cc
JEANCARLO <149130487+daniel102sa1@users.noreply.github.com>
2025-10-02 02:02:25 -0600

    Revert "Feature/api catalogos (#36)"
    This reverts commit fa336594091e5a94f0345b1c0344074331c5e16a.

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,9 +1,7 @@
+# Modelos que otros modelos necesitan para resolver FKs
 from .cat_tipo_articulo import CatTipoArticulo
 from .estado_articulo import EstadoArticulo
 from .estado_solicitud import EstadoSolicitud
-from .estado_prestamo import EstadoPrestamo      # ← Debe estar
-from .estado_pago import EstadoPago              # ← Debe estar
-from .estado_inventario import EstadoInventario  # ← Debe estar
 from .solicitud import Solicitud
 from .articulo import Articulo
 from .articulo_foto import ArticuloFoto
